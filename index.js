7c352e9fb7cb49048038b1c00f7868afa47fd187
Melvin San Jose <sanjose.melvin@gmail.com>
2025-12-09 14:05:46 +0800

    Complete exercise 13.5 and 13.6

--- index.js
+++ index.js
@@ -1,80 +1,22 @@
-require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+const blogsRouter = require('./controllers/blogs')
+
 const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
-
-class Blog extends Model {}
-Blog.init(
-	{
-		id: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true,
-		},
-		author: {
-			type: DataTypes.TEXT,
-		},
-		url: {
-			type: DataTypes.TEXT,
-			allowNull: false,
-		},
-		title: {
-			type: DataTypes.TEXT,
-			allowNull: false,
-		},
-		likes: {
-			type: DataTypes.INTEGER,
-			defaultValue: 0,
-		},
-	},
-	{
-		sequelize,
-		underscored: true,
-		timestamps: false,
-		modelName: 'blog',
-	}
-)
-
-Blog.sync()
-
-app.get('/api/blogs', async (_req, res) => {
-	const blogs = await Blog.findAll()
-	res.json(blogs)
-})
-
-app.get('/api/blogs/:id', async (req, res) => {
-	const blog = await Blog.findByPk(req.params.id)
-	if (blog) {
-		res.json(blog)
-	} else res.status(404).end()
-})
-
-app.post('/api/blogs', async (req, res) => {
-	try {
-		const blog = await Blog.create(req.body)
-		return res.json(blog)
-	} catch (error) {
-		return res.status(400).json({ error })
-	}
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-	const blogToDelete = await Blog.findByPk(req.params.id)
-	if (blogToDelete) {
-		await blogToDelete.destroy()
-		res.status(204).end()
-	} else res.status(404).end()
-})
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-})
+const start = async () => {
+	await connectToDatabase()
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`)
+	})
+}
 
 app.on('error', (error) => {
 	console.error('Server error:', error)
 })
+
+start()
