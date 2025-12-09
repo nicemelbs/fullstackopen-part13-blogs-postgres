38ef75e91ee2e71a5eb4ceee62738c180fcd8c1a
Melvin San Jose <sanjose.melvin@gmail.com>
2025-12-09 14:35:14 +0800

    Complete exercise 13.7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,6 +3,13 @@ const { Blog } = require('../models')
 
 const blogFinder = async (req, res, next) => {
 	req.blog = await Blog.findByPk(req.params.id)
+
+	if (!req.blog) {
+		res
+			.status(404)
+			.send({ error: `Blog with id=${req.params.id} not found.` })
+			.end()
+	}
 	next()
 }
 
@@ -12,32 +19,34 @@ router.get('/', async (_req, res) => {
 })
 
 router.get('/:id', blogFinder, async (req, res) => {
-	if (req.blog) {
-		res.json(req.blog)
-	} else res.status(404).end()
+	res.json(req.blog)
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
 	try {
 		const blog = await Blog.create(req.body)
 		return res.json(blog)
 	} catch (error) {
-		return res.status(400).json({ error })
+		// return res.status(400).json({ error })
+		res.status(400)
+		next(error)
 	}
 })
 
 router.put('/:id', blogFinder, async (req, res) => {
-	if (req.blog) {
-		req.blog.likes = req.body.likes
+	if (!req.body?.likes || isNaN(req.body.likes)) {
+		res.status(400)
+		throw new Error('new `likes` value invalid or missing')
+	} else {
+		req.blog.likes = Number(req.body.likes)
 		await req.blog.save()
-		res.json(req.blog)
-	} else req.status(404).end()
+
+		res.status(200).json(req.blog)
+	}
 })
 
 router.delete('/:id', blogFinder, async (req, res) => {
-	if (req.blog) {
-		await req.blog.destroy()
-		res.status(204).end()
-	} else res.status(404).end()
+	await req.blog.destroy()
+	res.status(204).end()
 })
 module.exports = router
--- index.js
+++ index.js
@@ -8,15 +8,25 @@ const app = express()
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: 'Unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+	console.error(`${error.name}: ${error.message}`)
+	next()
+}
+app.use(errorHandler)
+
 const start = async () => {
 	await connectToDatabase()
 	app.listen(PORT, () => {
 		console.log(`Server running on port ${PORT}`)
 	})
+	app.on('error', (error) => {
+		console.error('Server error:', error)
+	})
 }
-
-app.on('error', (error) => {
-	console.error('Server error:', error)
-})
 
 start()
