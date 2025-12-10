3ac094d4a4acbf1bcde214f56b96d46770120e57
Melvin San Jose <sanjose.melvin@gmail.com>
2025-12-10 20:18:42 +0800

    Complete exercise 13.23

--- controllers/users.js
+++ controllers/users.js
@@ -33,6 +33,20 @@ router.post('/', async (req, res, next) => {
 })
 
 router.get('/:id', async (req, res) => {
+	const where = {}
+	if (req.query.read) {
+		switch (req.query.read) {
+			case 'true':
+				where.read = true
+				break
+			case 'false':
+				where.read = false
+			default:
+				console.log(
+					`\nWARNING: query param \`read\` did not match either true or false. Found \`${req.query.read}\` Ignoring filter.\n`
+				)
+		}
+	}
 	const user = await User.findByPk(req.params.id, {
 		attributes: ['username', 'name'],
 		include: [
@@ -40,7 +54,7 @@ router.get('/:id', async (req, res) => {
 				model: Blog,
 				as: 'readings',
 				attributes: { exclude: ['createdAt', 'updatedAt'] },
-				through: { attributes: ['read', 'id'] },
+				through: { attributes: ['read', 'id'], where },
 			},
 		],
 	})
